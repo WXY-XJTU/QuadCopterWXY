ac04b72898ec7e4a938e15202b97c9d89f8014d3
xiayu0813 <xia_yu0813@126.com>
2014-08-28 15:26:09 +0800

    修改配置，完成LED程序的下载测试

--- User/main.c
+++ User/main.c
@@ -8,22 +8,36 @@
 GPIO_InitTypeDef GPIO_InitStructure;
 
 
+void delay(int delaytime)
+{
+    int i = 0;
+    while(--delaytime)
+    {
+        for(i = 0; i<5000;i++){}
+    }
+}
+
+
 int main(void)
 {
 
-       
   /* GPIOD Periph clock enable */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
   /* Configure PD0 and PD2 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 
   while (1)
   {
-  	 GPIO_SetBits(GPIOD, GPIO_Pin_9 | GPIO_Pin_15);
+    GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    delay(1000);
+      GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+      delay(1000);
+      
   }
 }
+
